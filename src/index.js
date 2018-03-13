e09176b145167d146e62ae824e9f27e3708e033a
alvis <alvisisme@163.com>
2018-03-13 14:38:38 +0800

    1. add lz77 source code add test. 2. update project config.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,249 @@
-import Cat from './cat.js';
-import Dog from './dog.js';
-export { Dog, Cat };
+// - - -
+// # LZ77
+// ** A minimal LZ77 [de]compressor **
+// let LZ77;
+// ##### Private Variables
+let
+  self = {},
+  settings = {
+    refPrefix: '`',
+    refIntBase: 96,
+    refIntFloorCode: ' '.charCodeAt(0),
+    refIntCeilCode: undefined,
+    maxStringDistance: undefined,
+    minStringLength: 5,
+    maxStringLength: undefined,
+    defaultWindow: 144,
+    maxWindow: undefined,
+    windowLength: undefined
+  }
+
+;
+
+// ##### Public Variables
+
+// ##### Private Methods
+// #### each()
+// >`@param obj [collection]` our source collection
+// >`@param iterator [function]` the function that will be called for each element in the collection
+// >`@param context [object]` the context our iterator should operate within
+//
+// essentially copied from underscore.js
+let each = function (obj, iterator, context) {
+  let breaker = {};
+
+  if (obj === null) {
+    return;
+  }
+  if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
+    obj.forEach(iterator, context);
+  } else if (obj.length === +obj.length) {
+    for (let i = 0, l = obj.length; i < l; i++) {
+      if (iterator.call(context, obj[i], i, obj) === breaker) {
+        return;
+      }
+    }
+  } else {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (iterator.call(context, obj[key], key, obj) === breaker) {
+          return;
+        }
+      }
+    }
+  }
+};
+
+// #### extend()
+// >`@param obj [object]` our destination object
+// >`@params * [object(s)]` objects that will overwrite the data in the destination object, in order
+//
+// essentially copied from underscore.js
+let extend = function (obj) {
+  each(Array.prototype.slice.call(arguments, 1), function (source) {
+    if (source) {
+      for (let prop in source) {
+        obj[prop] = source[prop];
+      }
+    }
+  });
+  return obj;
+};
+
+let setup = function (params) {
+  params = params || {};
+  settings = extend(settings, params);
+
+  settings.refIntCeilCode = settings.refIntFloorCode + settings.refIntBase - 1;
+  settings.maxStringDistance = Math.pow(settings.refIntBase, 2) - 1;
+  settings.maxStringLength = Math.pow(settings.refIntBase, 1) - 1 + settings.minStringLength;
+  settings.maxWindow = settings.maxStringDistance + settings.minStringLength;
+};
+
+let encodeRefInt = function (value, width) {
+  if ((value >= 0) && (value < (Math.pow(settings.refIntBase, width) - 1))) {
+    let encoded = '';
+
+    while (value > 0) {
+      encoded = (String.fromCharCode((value % settings.refIntBase) + settings.refIntFloorCode)) + encoded;
+      value = Math.floor(value / settings.refIntBase);
+    }
+    let missingLength = width - encoded.length;
+    let i = 0;
+
+    for (; i < missingLength; i++) {
+      encoded = String.fromCharCode(settings.refIntFloorCode) + encoded;
+    }
+    return encoded;
+  }
+  throw new Error('Reference int out of range: ' + value + ' (width = ' + width + ')');
+
+};
+
+let encodeRefLength = function (length) {
+  return encodeRefInt(length - settings.minStringLength, 1);
+};
+
+let decodeRefInt = function (data, width) {
+  let
+    value = 0,
+    charCode,
+    i = 0
+
+  ;
+  for (; i < width; i++) {
+    value *= settings.refIntBase;
+    charCode = data.charCodeAt(i);
+    if ((charCode >= settings.refIntFloorCode) && (charCode <= settings.refIntCeilCode)) {
+      value += charCode - settings.refIntFloorCode;
+    } else {
+      throw new Error('Invalid char code in reference int: ' + charCode);
+    }
+  }
+  return value;
+};
+
+let decodeRefLength = function (data) {
+  return decodeRefInt(data, 1) + settings.minStringLength;
+};
+
+// ##### Public Methods
+
+// #### LZ77.compress()
+// >`@param source [string]` the source string we will be compressing
+// >`@param params [object]` this allows us to overwrite defaults at call-time
+//
+// This is our compression method, taking the input string (and allowing for call-time
+// paramters) and returning the compressed representation
+self.compress = function (source, params) {
+  if (Object.prototype.toString.call(source) !== '[object String]') {
+    return false;
+  }
+
+  setup(params);
+
+  let windowLength = settings.windowLength || settings.defaultWindow;
+
+  if (windowLength > settings.maxWindow) {
+    throw new Error('Window length too large');
+  }
+
+  let
+    compressed = '',
+    pos = 0,
+    lastPos = source.length - settings.minStringLength
+
+  ;
+
+  while (pos < lastPos) {
+    let
+      searchStart = Math.max(pos - windowLength, 0),
+      matchLength = settings.minStringLength,
+      foundMatch = false,
+      bestMatch = {
+        distance: settings.maxStringDistance,
+        length: 0
+      },
+      newCompressed = null,
+      isValidMatch,
+      realMatchLength
+
+    ;
+
+    while ((searchStart + matchLength) < pos) {
+      isValidMatch = ((source.substr(searchStart, matchLength) === source.substr(pos, matchLength)) &&
+        (matchLength < settings.maxStringLength));
+      if (isValidMatch) {
+        matchLength++;
+        foundMatch = true;
+      } else {
+        realMatchLength = matchLength - 1;
+        if (foundMatch && (realMatchLength > bestMatch.length)) {
+          bestMatch.distance = pos - searchStart - realMatchLength;
+          bestMatch.length = realMatchLength;
+        }
+        matchLength = settings.minStringLength;
+        searchStart++;
+        foundMatch = false;
+      }
+    }
+
+    if (bestMatch.length) {
+      newCompressed = settings.refPrefix + encodeRefInt(bestMatch.distance, 2) + encodeRefLength(bestMatch.length);
+      pos += bestMatch.length;
+    } else {
+      if (source.charAt(pos) !== settings.refPrefix) {
+        newCompressed = source.charAt(pos);
+      } else {
+        newCompressed = settings.refPrefix + settings.refPrefix;
+      }
+      pos++;
+    }
+    compressed += newCompressed;
+  }
+
+  return compressed + source.slice(pos).replace(/`/g, '``');
+};
+
+// #### LZ77.decompress()
+// >`@param source [string]` the source string of compressed data
+// >`@param params [object]` this allows us to overwrite defaults at call-time
+//
+// decompression method, taking the compressed data (as a string, and allowing for
+// call-time paramters) and returning the decompressed data
+self.decompress = function (source, params) {
+  if (Object.prototype.toString.call(source) !== '[object String]') {
+    return false;
+  }
+
+  let
+    decompressed = '',
+    pos = 0,
+    currentChar,
+    nextChar,
+    distance,
+    length;
+
+  setup(params);
+  while (pos < source.length) {
+    currentChar = source.charAt(pos);
+    if (currentChar !== settings.refPrefix) {
+      decompressed += currentChar;
+      pos++;
+    } else {
+      nextChar = source.charAt(pos + 1);
+      if (nextChar !== settings.refPrefix) {
+        distance = decodeRefInt(source.substr(pos + 1, 2), 2);
+        length = decodeRefLength(source.charAt(pos + 3));
+        decompressed += decompressed.substr(decompressed.length - distance - length, length);
+        pos += settings.minStringLength - 1;
+      } else {
+        decompressed += settings.refPrefix;
+        pos += 2;
+      }
+    }
+  }
+  return decompressed;
+};
+
+export default self;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,32 +1,38 @@
 /* global describe, it, before */
 
 import chai from 'chai';
-import {Cat, Dog} from '../lib/webpack-library-starter.js';
-
-chai.expect();
-
+import lz77 from '../lib/lz77.js';
 const expect = chai.expect;
 
-let lib;
+describe('LZ77', () => {
+  let source =     "Sanskrit: काचं शक्नोम्यत्तुम् । नोपहिनस्ति माम् ॥ Sanskrit (standard transcription): kācaṃ śaknomyattum; nopahinasti mām. Classical Greek: ὕαλον ϕαγεῖν δύναμαι· τοῦτο οὔ με βλάπτει. Greek (monotonic): Μπορώ να φάω σπασμένα γυαλιά χωρίς να πάθω τίποτα. Greek (polytonic): Μπορῶ νὰ φάω σπασμένα γυαλιὰ χωρὶς νὰ πάθω τίποτα.  Etruscan: (NEEDED) Latin: Vitrum edere possum; mihi non nocet. Old French: Je puis mangier del voirre. Ne me nuit. French: Je peux manger du verre, ça ne me fait pas mal. Provençal / Occitan: Pòdi manjar de veire, me nafrariá pas. Québécois: J'peux manger d'la vitre, ça m'fa pas mal. Walloon: Dji pou magnî do vêre, çoula m' freut nén må.  Champenois: (NEEDED)  Lorrain: (NEEDED) Picard: Ch'peux mingi du verre, cha m'foé mie n'ma.  Corsican/Corsu: (NEEDED)  Jèrriais: (NEEDED) Kreyòl Ayisyen (Haitï): Mwen kap manje vè, li pa blese'm.";
+  let compressed = "Sanskrit: काचं शक्नोम्यत्तुम् । नोपहिनस्ति माम् ॥ ` J# (standard transcription): kācaṃ śaknomyattum; nopahinasti mām. Classical Greek: ὕαλον ϕαγεῖν δύναμαι· τοῦτο οὔ με βλάπτει.` L! (monotonic): Μπορώ να φάω σπασμένα γυαλιά χωρίς να πάθω τίποτα` ]$poly` Z'ῶ νὰ` S.ὰ χωρὶς νὰ` X) Etruscan: (NEEDED) Latin: Vitrum edere possum; mihi non nocet. Old French: Je puis mangier del voirre. Ne me nuit.` C(eux` K er du verre, ça n` K fait pas mal. Provençal / Occitan: Pòdi manjar de veire,`!' afrariá pas. Québécois: J'`!)('la vit`!1\"m'fa`!(%Walloon: Dji pou magnî do vê` L oula m' freut nén må.  Champen`!* (NEEDED)  Lorrain` (&Picard: Ch'peux mingi du verre, cha m'foé mie n'ma.  Corsican/Corsu` u'Jèrria`!((Kreyòl Ayisyen (Haitï): Mwen kap manje vè, li pa blese'm.";
 
-describe('Given an instance of my Cat library', () => {
-  before(() => {
-    lib = new Cat();
-  });
-  describe('when I need the name', () => {
-    it('should return the name', () => {
-      expect(lib.name).to.be.equal('Cat');
+  describe('compress', function() {
+    it('Compresses the provided textual content.', function() {
+      let test = lz77.compress(source);
+
+      expect(test).to.equal(compressed);
+    });
+
+    it('Fails if a string is not provided', function() {
+      let test = lz77.compress(['a', 'b']);
+
+      expect(test).to.be.false;
     });
   });
-});
 
-describe('Given an instance of my Dog library', () => {
-  before(() => {
-    lib = new Dog();
-  });
-  describe('when I need the name', () => {
-    it('should return the name', () => {
-      expect(lib.name).to.be.equal('Dog');
+  describe('decompress', function() {
+    it('Decompresses the provided content', function() {
+      let test = lz77.decompress(compressed);
+
+      expect(test).to.equal(source);
+    });
+
+    it('Fails if a string is not provided', function() {
+      let test = lz77.decompress({list: ['a', 'b']});
+
+      expect(test).to.be.false;
     });
   });
 });
